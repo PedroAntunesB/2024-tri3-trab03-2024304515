c44d7694102b2e290146b20e3739b6a92fd428b9
Pedro Antunes <pedrohantunnes@gmail.com>
2024-11-03 23:36:21 -0300

    trab 005-realizado

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # 2024-tri3-trab03-2024304515
 ## Descrição 
-galeria de fotos responsiva com um navegation, feito para a materia de web design e tem tanto o trab 003 quanto o 004. 
+galeria de fotos responsiva com um navegation, feito para a materia de web design e tem tanto o trab 003 quanto o 004.  
+
+Realizado o trab 005 que consistia em criar um modal no inicio do site e um modal para quando clicar nas imagens.
--- assets/main.js
+++ assets/main.js
@@ -1,11 +1,18 @@
-var images = document.querySelectorAll('main img')
-images.forEach(function (image) {
-    image.addEventListener('click', function (event) {
-        event.target.classList.toggle('full-screen')
-        if (event.target.classList.contains('full-screen')) {
-            event.target.style.transform = 'translate(-50%, -50%) scale(' + (window.innerHeight / event.target.naturalHeight) + ')'
-        } else {
-            event.target.style.transform = 'none'
-        }
+const galleryImageModal = document.querySelector('.gallery-image-modal')
+const galleryBtClose = galleryImageModal.querySelector('.bt-close')
+
+galleryImageModal.showModal()
+galleryImageModal.addEventListener('click', () => {
+    galleryImageModal.close()
+})
+
+
+const img = document.querySelectorAll('img')
+let html = ''
+img.forEach((img) => {
+    img.addEventListener('click', () => {
+        galleryImageModal.showModal()
+        html = `<img src="${img.src}" alt="${img.alt}" style="width: 20vw; height: auto;">`
+        galleryImageModal.innerHTML = html
     })
 })
