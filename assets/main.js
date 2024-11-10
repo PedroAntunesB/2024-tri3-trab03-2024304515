3720e32e29ed6954992102241e95d630caa36b3a
PedroAntunes <pedrohantunnes@gmail.com>
2024-11-10 17:16:32 -0300

    melhoras no js e css

--- assets/main.js
+++ assets/main.js
@@ -8,11 +8,29 @@ galleryImageModal.addEventListener('click', () => {
 
 
 const img = document.querySelectorAll('img')
-let html = ''
+const content = document.querySelector('.content')
+let antes = document.querySelector('.antes')
 img.forEach((img) => {
     img.addEventListener('click', () => {
+        content.innerHTML = `<img src="${img.src}" alt="${img.alt}" style="width: 20vw; height: auto;">`
         galleryImageModal.showModal()
-        html = `<img src="${img.src}" alt="${img.alt}" style="width: 20vw; height: auto;">`
-        galleryImageModal.innerHTML = html
     })
 })
+
+const galleryContent = galleryImageModal.querySelector('.content')
+const imgs = document.querySelectorAll('.fotos img')
+const galleryBtNext = galleryImageModal.querySelector('.depois')
+let nextImg = null //vai pegar a proxima imagem
+
+galleryBtNext.addEventListener('click', () => {
+    galleryImageModal.close()
+    nextImg.click()
+})
+
+imgs.forEach(img => {
+  img.addEventListener('click', () => {
+    nextImg = img.parentNode.nextElementSibling.querySelector('img') // DICASSSS
+    galleryContent.innerHTML = `<img src="${nextImg.src}" alt="${nextimg.alt}">`
+    galleryImageModal.showModal()
+  })
+})  
